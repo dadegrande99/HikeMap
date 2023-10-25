008fe78d804ba211579d36fe0a38bed4eab832ce
andrew3 <a.messa@campus.unimib.it>
2023-10-25 16:48:38 +0200

    Change some files

--- app/src/main/java/com/usi/hikemap/ui/fragment/GoFragment.java
+++ app/src/main/java/com/usi/hikemap/ui/fragment/GoFragment.java
@@ -1,4 +1,4 @@
-package com.usi.hikemap.ui.go;
+package com.usi.hikemap.ui.fragment;
 
 import android.os.Bundle;
 import android.view.LayoutInflater;
@@ -10,22 +10,23 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.usi.hikemap.databinding.FragmentDashboardBinding;
+import com.usi.hikemap.databinding.FragmentGoBinding;
+import com.usi.hikemap.ui.viewmodel.GoViewModel;
 
 public class GoFragment extends Fragment {
 
-    private FragmentDashboardBinding binding;
+    private FragmentGoBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        GoViewModel dashboardViewModel =
+        GoViewModel goViewModel =
                 new ViewModelProvider(this).get(GoViewModel.class);
 
-        binding = FragmentDashboardBinding.inflate(inflater, container, false);
+        binding = FragmentGoBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView textView = binding.textDashboard;
-        dashboardViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
+        final TextView textView = binding.textGo;
+        goViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
         return root;
     }
 
--- app/src/main/java/com/usi/hikemap/ui/viewmodel/GoViewModel.java
+++ app/src/main/java/com/usi/hikemap/ui/viewmodel/GoViewModel.java
@@ -1,16 +1,16 @@
-package com.usi.hikemap.ui.notifications;
+package com.usi.hikemap.ui.viewmodel;
 
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-public class NotificationsViewModel extends ViewModel {
+public class GoViewModel extends ViewModel {
 
     private final MutableLiveData<String> mText;
 
-    public NotificationsViewModel() {
+    public GoViewModel() {
         mText = new MutableLiveData<>();
-        mText.setValue("This is notifications fragment");
+        mText.setValue("This is go fragment");
     }
 
     public LiveData<String> getText() {
