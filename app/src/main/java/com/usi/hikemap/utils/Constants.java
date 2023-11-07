032915ccf8e5b0e14870483c27777cc9a37d6b6e
andrew-unimib <76683808+andrew-unimib@users.noreply.github.com>
2023-11-07 19:11:16 +0100

    Add Utils and Font

---
+++ app/src/main/java/com/usi/hikemap/utils/Constants.java
@@ -0,0 +1,14 @@
+package com.usi.hikemap.utils;
+
+public class Constants {
+
+    public static final int RC_SIGN_IN = 100;
+    public static String USER_COLLECTION = "users";
+
+    public static String DEFAULT_WEB_CLIENT_ID = "35606227968-7fthbhiudp0tnpn9tae31qaln3d605j2.apps.googleusercontent.com";
+    public static String FIREBASE_DATABASE_URL = "https://hikemap-e9d2a-default-rtdb.firebaseio.com/";
+
+    public static String URL_LINK_EMAIL ="https://hikemap-e9d2a.firebaseapp.com/__/auth/action?mode=verifyEmail&oobCode=fDaMeWEq62kAEyc39IeKW0mq3vOi0Pge-zGbszAXCZgAAAF9V0SOmg&apiKey=AIzaSyDqkEUl0euzeZog1uF4dorcp7z2aE-DXH8&lang=en";
+
+
+}
