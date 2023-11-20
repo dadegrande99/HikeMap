e3d298bb88c51d94f1547ec9a4fecabd1e2f96a1
dadegrande99 <davidegrandesso@gmail.com>
2023-11-20 12:35:27 +0100

    Blank profile

--- app/src/main/java/com/usi/hikemap/MainActivity.java
+++ app/src/main/java/com/usi/hikemap/MainActivity.java
@@ -27,7 +27,7 @@ public class MainActivity extends AppCompatActivity {
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.navigation_profile, R.id.navigation_search, R.id.navigation_go)
+                R.id.navigation_search, R.id.navigation_go, R.id.navigation_profile)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
