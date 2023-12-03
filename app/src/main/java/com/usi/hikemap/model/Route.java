e0b4b62b96ee7343d35c88f6ec4bb589ced97d4f
Andrea Messa <andrea.messa@usi.ch>
2023-12-03 04:23:45 +0100

    add route class in package model

---
+++ app/src/main/java/com/usi/hikemap/model/Route.java
@@ -0,0 +1,84 @@
+package com.usi.hikemap.model;
+
+import com.google.android.gms.maps.model.LatLng;
+
+import java.util.List;
+
+public class Route {
+
+    private int id, subRoute;
+    private String timestamp;
+    private double altitude, longitude, latitude;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getSubRoute() {
+        return subRoute;
+    }
+
+    public void setSubRoute(int subRoute) {
+        this.subRoute = subRoute;
+    }
+
+    public String getTimestamp() {
+        return timestamp;
+    }
+
+    public void setTimestamp(String timestamp) {
+        this.timestamp = timestamp;
+    }
+
+    public double getAltitude() {
+        return altitude;
+    }
+
+    public void setAltitude(double altitude) {
+        this.altitude = altitude;
+    }
+
+    public double getLongitude() {
+        return longitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public double getLatitude() {
+        return latitude;
+    }
+
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public Route(int id, int subRoute, String timestamp, double altitude, double longitude, double latitude) {
+        this.id = id;
+        this.subRoute = subRoute;
+        this.timestamp = timestamp;
+        this.altitude = altitude;
+        this.longitude = longitude;
+        this.latitude = latitude;
+    }
+
+    @Override
+    public String toString() {
+        return "Route{" +
+                "id=" + id +
+                ", subRoute=" + subRoute +
+                ", timestamp='" + timestamp + '\'' +
+                ", altitude=" + altitude +
+                ", longitude=" + longitude +
+                ", latitude=" + latitude +
+                '}';
+    }
+
+
+}
+
