c99b6cde06564f071c945feeebd54f8ef640e573
Andrea Messa <andrea.messa@usi.ch>
2023-12-16 21:16:58 +0100

    Adjust HikeDetailsFragment

--- app/src/main/java/com/usi/hikemap/ui/fragment/HikeDetailsFragment.java
+++ app/src/main/java/com/usi/hikemap/ui/fragment/HikeDetailsFragment.java
@@ -48,7 +48,7 @@ public class HikeDetailsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        View root = inflater.inflate(R.layout.fragment_go_details, container, false);
+        View root = inflater.inflate(R.layout.fragment_hike_details, container, false);
 
 
         Bundle args = getArguments();
